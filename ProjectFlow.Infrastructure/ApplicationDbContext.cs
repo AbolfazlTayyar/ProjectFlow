db51657aa604171003f484c9e25c29bc09438f38
Abolfazl Tayyar <Abolfazltayyar@gmail.com>
2025-10-18 22:45:27 +0330

    feature: add applicationDbContext class to infrastructure project

---
+++ ProjectFlow.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ProjectFlow.Domain.Abstractions;
+
+namespace ProjectFlow.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
+{
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+
+    }
+}
--- ProjectFlow.Infrastructure/GlobalUsings.cs
+++ ProjectFlow.Infrastructure/GlobalUsings.cs
@@ -1,2 +1,3 @@
-﻿global using Microsoft.Extensions.Configuration;
+﻿global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
