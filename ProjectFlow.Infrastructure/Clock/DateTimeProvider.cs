d258c887232f98c628b3fe9449bf317db765186c
Abolfazl Tayyar <Abolfazltayyar@gmail.com>
2025-10-18 22:31:26 +0330

    feature: add infrastructure project

---
+++ ProjectFlow.Infrastructure/Clock/DateTimeProvider.cs
@@ -0,0 +1,8 @@
+﻿using ProjectFlow.Application.Abstractions.Clock;
+
+namespace ProjectFlow.Infrastructure.Clock;
+
+internal sealed class DateTimeProvider : IDateTimeProvider
+{
+    public DateTime UtcNow => DateTime.UtcNow;
+}
---
+++ ProjectFlow.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using ProjectFlow.Application.Abstractions.Clock;
+using ProjectFlow.Application.Abstractions.Email;
+using ProjectFlow.Infrastructure.Clock;
+using ProjectFlow.Infrastructure.Email;
+
+namespace ProjectFlow.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+
+        services.AddTransient<IEmailService, EmailService>();
+
+        return services;
+    }
+}
