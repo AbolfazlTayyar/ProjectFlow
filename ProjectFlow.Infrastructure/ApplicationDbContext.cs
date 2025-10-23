97980a6ea308ee09b225892f0a1ae1e9f7551344
Abolfazl Tayyar <Abolfazltayyar@gmail.com>
2025-10-23 12:07:51 +0330

    feature: publish domain events using the unit of work

--- ProjectFlow.Infrastructure/ApplicationDbContext.cs
+++ ProjectFlow.Infrastructure/ApplicationDbContext.cs
@@ -4,10 +4,12 @@ namespace ProjectFlow.Infrastructure;
 
 public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDbContext(DbContextOptions options)
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
         : base(options)
     {
-
+        _publisher = publisher;
     }
 
     override protected void OnModelCreating(ModelBuilder modelBuilder)
@@ -15,5 +17,35 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(x => x.Entity)
+            .SelectMany(x =>
+            {
+                var domainEvents = x.GetDomainEvents();
+
+                x.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
