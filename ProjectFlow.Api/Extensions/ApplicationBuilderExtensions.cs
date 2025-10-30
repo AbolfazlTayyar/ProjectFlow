124569164b656e7d10493e65c4328cbde6b98073
Abolfazl Tayyar <Abolfazltayyar@gmail.com>
2025-10-30 11:46:28 +0330

    feature: add ExceptionHandlingMiddleware

--- ProjectFlow.Api/Extensions/ApplicationBuilderExtensions.cs
+++ ProjectFlow.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ProjectFlow.Api.Middlewares;
 using ProjectFlow.Infrastructure;
 
 namespace ProjectFlow.Api.Extensions;
@@ -12,5 +13,10 @@ public static class ApplicationBuilderExtensions
         using var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
 
         dbContext.Database.Migrate();
+    }
+
+    public static void UseCustomExceptionHandling(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlingMiddleware>();
     }
 }
